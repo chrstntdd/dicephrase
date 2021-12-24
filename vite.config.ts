be35a325f9047695ea449734007f66b5e140abf1
Christian Todd <christian.todd7@gmail.com>
2021-12-24 10:13:30 -0500

    Add node types and use https in dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+import { readFileSync } from "fs"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react()]
+  plugins: [vanillaExtractPlugin(), react()],
+  server: {
+    https: {
+      key: readFileSync("localhost-key.pem"),
+      cert: readFileSync("localhost.pem")
+    }
+  }
 })

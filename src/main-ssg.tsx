80d16d6aa39dfbae516d1bf0cf66fd4743267d67
Christian Todd <christian.todd7@gmail.com>
2022-01-08 10:53:58 -0500

    Upgrade deps to latest and fix hydration script generation

--- src/main-ssg.tsx
+++ src/main-ssg.tsx
@@ -3,12 +3,12 @@
  * Server side app entry to get a plain ol html string for prerendering
  * or just SSR.
  */
-import { renderToStringAsync, generateHydrationScript } from "solid-js/web"
+import { renderToStringAsync, HydrationScript } from "solid-js/web"
 import { App } from "./app"
 
 async function render(url: string) {
   let html = await renderToStringAsync(() => <App url={url} />)
-  let hydrationScript = generateHydrationScript()
+  let hydrationScript = await renderToStringAsync(() => <HydrationScript />)
 
   return { html, hydrationScript }
 }

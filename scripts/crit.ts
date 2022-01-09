91cd5819b1bec41e7e9c4b63f1b42b3ddacbc717
Christian Todd <christian.todd7@gmail.com>
2022-01-08 22:30:02 -0500

    CRITICAL CSS

---
+++ scripts/crit.ts
@@ -0,0 +1,38 @@
+import { writeFile } from "fs/promises"
+import { resolve } from "path"
+
+import crit from "critical"
+
+import { walkSync } from "./walk"
+
+let srcHtml = Array.from(
+  walkSync("./dist", {
+    includeDirs: false,
+    includeFiles: true,
+    filter: (n) => n.endsWith(".html")
+  })
+)
+let css = Array.from(
+  walkSync("./dist", {
+    includeDirs: false,
+    includeFiles: true,
+    filter: (n) => n.endsWith(".css")
+  })
+)
+
+let x = await Promise.all(
+  srcHtml.map(async ({ name, data }) => {
+    let htmlWithInlinedCSS: string = (
+      await crit.generate({
+        base: "/dist",
+        css: css.map(({ name }) => name),
+        inline: true,
+        src: name
+      })
+    ).html
+
+    return writeFile(resolve(name), htmlWithInlinedCSS, "utf-8")
+  })
+)
+
+console.log({ x })

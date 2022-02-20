3c4b2bedd52d87173f61c0c17ab7728daa9f117b
Christian Todd <christian.todd7@gmail.com>
2022-02-20 09:28:06 -0500

    Re-add logic to extract critical CSS to inline in head to avoid FOUC (#20)

--- apps/main/scripts/crit.ts
+++ apps/main/scripts/crit.ts
@@ -1,10 +1,10 @@
 import { writeFile } from "fs/promises"
 import { resolve } from "path"
 
+import crit from "critical"
 import { minify } from "html-minifier-terser"
 
 import { walkSync } from "./walk"
-import { readFileSync } from "fs"
 
 let srcHtml = Array.from(
   walkSync("./dist", {
@@ -13,12 +13,26 @@ let srcHtml = Array.from(
     filter: (n) => n.endsWith(".html")
   })
 )
+let css = Array.from(
+  walkSync("./dist", {
+    includeDirs: false,
+    includeFiles: true,
+    filter: (n) => n.endsWith(".css")
+  })
+)
 
 await Promise.all(
   srcHtml.map(async ({ name, data }) => {
-    let html = readFileSync(name, "utf-8")
+    let htmlWithInlinedCSS: string = (
+      await crit.generate({
+        base: "/dist",
+        css: css.map(({ name }) => name),
+        inline: true,
+        src: name
+      })
+    ).html
 
-    let minifiedHTML = await minify(html, {
+    let minifiedHTML = await minify(htmlWithInlinedCSS, {
       collapseBooleanAttributes: true,
       collapseWhitespace: true,
       minifyCSS: true,

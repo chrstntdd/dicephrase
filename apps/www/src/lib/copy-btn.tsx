cd3614e5cf9ec0020ea2e425ab2d02b8ae20adc2
Christian Todd <christian.todd7@gmail.com>
2022-06-05 18:31:21 -0400

    feat: :sparkles: Upgrade deps, fresh size snapshot, prefer class over className

--- apps/www/src/lib/copy-btn.tsx
+++ apps/www/src/lib/copy-btn.tsx
@@ -31,7 +31,7 @@ export default function CopyBtn(props: {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 48 48"
-          className={styles.copyIcon}
+          class={styles.copyIcon}
         >
           <path
             fill="currentColor"

32dfd61a8ce9aedd7d70d2c1a0c313c69fa4c7a4
Christian Todd <christian.todd7@gmail.com>
2022-06-05 09:59:01 -0400

    chore: :arrow_up: Upgrade TS, vite, vitest to latest and adjust coverage configuration

--- pkgs/gen-utils/src/gen.spec.ts
+++ pkgs/gen-utils/src/gen.spec.ts
@@ -12,7 +12,7 @@ import {
   parse_count_val,
   make_separators,
   make_phrases
-} from "./Gen.gen"
+} from "."
 
 vi.stubGlobal("crypto", {
   // @ts-expect-error
@@ -27,8 +27,8 @@ beforeAll(() => {
   )
 })
 
-describe("combine_zip", () => {
-  test.concurrent("should combine the two arrays", () => {
+describe.concurrent("combine_zip", () => {
+  test("should combine the two arrays", () => {
     let a1 = ["1", "2", "3"]
     let a2 = ["a", "b", "c"]
 
@@ -38,7 +38,7 @@ describe("combine_zip", () => {
     expect(out.length).toEqual(a1.length + a2.length)
   })
 
-  test.concurrent("should ignore extra elements in the second array", () => {
+  test("should ignore extra elements in the second array", () => {
     let a1 = ["1", "2", "3"]
     let a2 = ["a", "b", "c", "d", "e", "f"]
 
@@ -50,111 +50,93 @@ describe("combine_zip", () => {
 })
 
 describe("make_wl_keys", () => {
-  test.concurrent(
-    "should return a list equal in size to the count param",
-    () => {
-      let count = 10
-      let out = make_wl_keys(count)
+  test("should return a list equal in size to the count param", () => {
+    let count = 10
+    let out = make_wl_keys(count)
 
-      expect(out.length).toEqual(count)
-    }
-  )
+    expect(out.length).toEqual(count)
+  })
 
-  test.concurrent(
-    "should return a list where each entry is a 5 digit number",
-    () => {
-      let count = 10
-      let out = make_wl_keys(count)
+  test("should return a list where each entry is a 5 digit number", () => {
+    let count = 10
+    let out = make_wl_keys(count)
 
-      let firstEntry = out[0]
+    let firstEntry = out[0]
 
-      expect(typeof firstEntry).toBe("string")
-      expect(out.every((k) => k.length === 5)).toBeTruthy()
-    }
-  )
+    expect(typeof firstEntry).toBe("string")
+    expect(out.every((k) => k.length === 5)).toBeTruthy()
+  })
 })
 
-describe("parse_count_val", () => {
-  test.concurrent("should return the value as a number if it exists", () => {
+describe.concurrent("parse_count_val", () => {
+  test("should return the value as a number if it exists", () => {
     let out = parse_count_val("3")
     expect(out).toEqual(3)
   })
 
-  test.concurrent(
-    "should return the fallback count if there is an error",
-    () => {
-      let out0 = parse_count_val("")
-      expect(out0).toEqual(PHRASE_COUNT_FALLBACK)
+  test("should return the fallback count if there is an error", () => {
+    let out0 = parse_count_val("")
+    expect(out0).toEqual(PHRASE_COUNT_FALLBACK)
 
-      // @ts-expect-error
-      let out1 = parse_count_val(NaN)
-      expect(out1).toEqual(PHRASE_COUNT_FALLBACK)
+    // @ts-expect-error
+    let out1 = parse_count_val(NaN)
+    expect(out1).toEqual(PHRASE_COUNT_FALLBACK)
 
-      let out2 = parse_count_val("abc")
-      expect(out2).toEqual(PHRASE_COUNT_FALLBACK)
+    let out2 = parse_count_val("abc")
+    expect(out2).toEqual(PHRASE_COUNT_FALLBACK)
 
-      // @ts-expect-error
-      let out3 = parse_count_val({})
-      expect(out3).toEqual(PHRASE_COUNT_FALLBACK)
+    // @ts-expect-error
+    let out3 = parse_count_val({})
+    expect(out3).toEqual(PHRASE_COUNT_FALLBACK)
 
-      // This should pass, but I'm not being careful enough on the ReScript side
-      // let out4 = parse_count_val(-99)
-      // expect(out4).toEqual(PHRASE_COUNT_FALLBACK)
-    }
-  )
+    // This should pass, but I'm not being careful enough on the ReScript side
+    // let out4 = parse_count_val(-99)
+    // expect(out4).toEqual(PHRASE_COUNT_FALLBACK)
+  })
 })
 
-describe("parse_qs_to_phrase_config", () => {
-  test.concurrent("works with known values", () => {
+describe.concurrent("parse_qs_to_phrase_config", () => {
+  test("works with known values", () => {
     let x = parse_qs_to_phrase_config("?phrase-count=9&separator=random")
 
     expect(x.count).toEqual(9)
     expect(x.sep).toEqual("random")
   })
 
-  test.concurrent(
-    "ignores out of bounds count values — returning the default",
-    () => {
-      let x0 = parse_qs_to_phrase_config("?phrase-count=1&separator=random")
+  test("ignores out of bounds count values — returning the default", () => {
+    let x0 = parse_qs_to_phrase_config("?phrase-count=1&separator=random")
 
-      expect(x0.count).toEqual(8)
-      expect(x0.sep).toEqual("random")
+    expect(x0.count).toEqual(8)
+    expect(x0.sep).toEqual("random")
 
-      let x1 = parse_qs_to_phrase_config("?phrase-count=11&separator=random")
+    let x1 = parse_qs_to_phrase_config("?phrase-count=11&separator=random")
 
-      expect(x1.count).toEqual(8)
-      expect(x1.sep).toEqual("random")
-    }
-  )
+    expect(x1.count).toEqual(8)
+    expect(x1.sep).toEqual("random")
+  })
 
-  test.concurrent(
-    "ignores unknown separator values — returning the default",
-    () => {
-      let x0 = parse_qs_to_phrase_config("?phrase-count=8&separator=f00")
+  test("ignores unknown separator values — returning the default", () => {
+    let x0 = parse_qs_to_phrase_config("?phrase-count=8&separator=f00")
 
-      expect(x0.count).toEqual(8)
-      expect(x0.sep).toEqual("random")
+    expect(x0.count).toEqual(8)
+    expect(x0.sep).toEqual("random")
 
-      let x1 = parse_qs_to_phrase_config("?phrase-count=8&separator=b00")
+    let x1 = parse_qs_to_phrase_config("?phrase-count=8&separator=b00")
 
-      expect(x1.count).toEqual(8)
-      expect(x1.sep).toEqual("random")
-    }
-  )
+    expect(x1.count).toEqual(8)
+    expect(x1.sep).toEqual("random")
+  })
 
-  test.concurrent(
-    "ignores unknown query params and returns the default",
-    () => {
-      let x = parse_qs_to_phrase_config("?booty=true")
+  test("ignores unknown query params and returns the default", () => {
+    let x = parse_qs_to_phrase_config("?booty=true")
 
-      expect(x.count).toEqual(8)
-      expect(x.sep).toEqual("random")
-    }
-  )
+    expect(x.count).toEqual(8)
+    expect(x.sep).toEqual("random")
+  })
 })
 
-describe("make_phrases", () => {
-  test.concurrent("should return an array of words", () => {
+describe.concurrent("make_phrases", () => {
+  test("should return an array of words", () => {
     let count = 4
     let out = make_phrases<string>(count, wl)
 
@@ -163,7 +145,7 @@ describe("make_phrases", () => {
   })
 })
 
-describe("make_separators", () => {
+describe.concurrent("make_separators", () => {
   // This test is flaky, but sorta gets the idea across
   test.skip("should return random separators when separator_kind is 'random'", () => {
     let count = 8
@@ -173,25 +155,19 @@ describe("make_separators", () => {
     expect(uniqueSeparators).not.toEqual(out)
   })
 
-  test.concurrent(
-    "should return an array of length 1 less than the count to splice between the words",
-    () => {
-      let count = 8
-      let out = make_separators("random", count)
+  test("should return an array of length 1 less than the count to splice between the words", () => {
+    let count = 8
+    let out = make_separators("random", count)
 
-      expect(out.length).toEqual(count - 1)
-    }
-  )
+    expect(out.length).toEqual(count - 1)
+  })
 
-  test.concurrent(
-    'Should return an array of identical chars when not "random"',
-    () => {
-      let count = 6
-      let sep = "~"
-      let out = make_separators(sep, count)
+  test('Should return an array of identical chars when not "random"', () => {
+    let count = 6
+    let sep = "~"
+    let out = make_separators(sep, count)
 
-      expect(out.length).toEqual(count - 1)
-      expect(out.every((c) => c === sep)).toBeTruthy()
-    }
-  )
+    expect(out.length).toEqual(count - 1)
+    expect(out.every((c) => c === sep)).toBeTruthy()
+  })
 })

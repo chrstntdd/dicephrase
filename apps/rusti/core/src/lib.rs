7f1d8c82ea2a54882d8177c0b32afe6f4d4ea7d7
Christian Todd <christian.todd7@gmail.com>
2023-01-21 10:38:03 -0500

    Refactor main lib to remove unused code, use reference types, and generate the wordlist keys as string in the iterator impl

--- apps/rusti/core/src/lib.rs
+++ apps/rusti/core/src/lib.rs
@@ -5,27 +5,14 @@ use rand::{
 };
 use std::{collections::HashMap, io};
 
-pub fn dicephrase(
-    count: usize,
-    separator_kind: String,
-) -> io::Result<(Vec<String>, Vec<String>, String)> {
-    let word_list = read_wl()?;
-    let separators = make_separators(count, &separator_kind);
-    let words = make_words(count, word_list);
-
-    let full_phrase = make_full_phrase(words.clone(), separators.clone());
-
-    Ok((words, separators, full_phrase))
-}
-
-pub fn make_full_phrase(words: Vec<String>, separators: Vec<String>) -> String {
+pub fn make_full_phrase(words: &Vec<String>, separators: &Vec<String>) -> String {
     let final_index = words.len() - 1;
     words
         .iter()
         .zip(separators.iter())
         .enumerate()
         .map(|(i, (word, sep))| {
-            let word = word.to_owned();
+            let word = word.clone();
             // Drop the dangling separator at the end.
             if i == final_index {
                 word
@@ -37,9 +24,9 @@ pub fn make_full_phrase(words: Vec<String>, separators: Vec<String>) -> String {
         .join("")
 }
 
-pub fn make_words(count: usize, word_list: HashMap<String, String>) -> Vec<String> {
-    let keys = make_wl_keys(count);
-    keys.iter()
+pub fn make_words(count: usize, word_list: &HashMap<String, String>) -> Vec<String> {
+    make_wl_keys(count)
+        .iter()
         .flat_map(|k| match word_list.get(k) {
             Some(word) => Some(word.clone()),
             _ => None,
@@ -52,12 +39,12 @@ struct WordListKeys {
 }
 
 impl Iterator for WordListKeys {
-    type Item = Vec<u8>;
+    type Item = Vec<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let die = Uniform::new_inclusive(1, 6);
         let roll = die.sample_iter(&mut self.rng);
-        let keys: Vec<u8> = roll.take(5).collect();
+        let keys: Vec<String> = roll.take(5).map(|x| x.to_string()).collect();
         Some(keys)
     }
 }
@@ -66,41 +53,39 @@ fn make_wl_keys(count: usize) -> Vec<String> {
     let rng = rand::thread_rng();
     WordListKeys { rng }
         .take(count)
-        .map(|e| e.iter().map(|x| x.to_string()).collect::<String>())
+        .map(|roll_result| roll_result.join(""))
         .collect()
 }
 
 const SEPARATOR_OPTS: [&'static str; 12] =
     ["_", ",", "!", "@", "*", "&", "^", "~", "-", ".", "$", "|"];
+const SEPARATOR_OPTS_LEN: usize = SEPARATOR_OPTS.len();
 
 pub fn make_separators(count: usize, separator_kind: &str) -> Vec<String> {
     // TODO: Figure out how to have count be `count - 1` and have the caller
     // zip the two uneven vectors - might be more work than just zipping the
     // two being equal by generating 1 extra element
-    let separators = if separator_kind == "random" {
-        let mut rng = rand::thread_rng();
-        let separator_opts_len = SEPARATOR_OPTS.len();
-
-        (0..count)
-            .into_iter()
-            .filter_map(|_| {
-                let random_index = rng.gen_range(0..separator_opts_len);
-                match SEPARATOR_OPTS.get(random_index) {
-                    Some(sep) => Some(sep.clone().to_owned()),
-                    _ => panic!("Unable to get random char"),
-                }
-            })
-            .collect()
-    } else {
-        separator_kind
+    match separator_kind {
+        "random" => {
+            let mut rng = rand::thread_rng();
+            (0..count)
+                .into_iter()
+                .filter_map(|_| {
+                    let random_index = rng.gen_range(0..SEPARATOR_OPTS_LEN);
+                    match SEPARATOR_OPTS.get(random_index) {
+                        Some(sep) => Some(sep.clone().to_owned()),
+                        _ => None,
+                    }
+                })
+                .collect()
+        }
+        _ => separator_kind
             .repeat(count)
             .split("")
             .map(|x| x.to_owned())
             .filter(|x| x.trim() != "")
-            .collect()
-    };
-
-    separators
+            .collect(),
+    }
 }
 
 pub fn read_wl() -> io::Result<HashMap<String, String>> {

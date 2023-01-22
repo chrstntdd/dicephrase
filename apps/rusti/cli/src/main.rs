c064508ee208bc94bdd20323cab3419ee1b93a39
Christian Todd <christian.todd7@gmail.com>
2023-01-21 19:05:38 -0500

    print dicephrase in cli scaffold

--- apps/rusti/cli/src/main.rs
+++ apps/rusti/cli/src/main.rs
@@ -1,10 +1,17 @@
 use std::io;
 
-use core_dicephrase::{self, dicephrase};
+use core_dicephrase::{make_full_phrase, make_separators, make_words, read_wl};
 
 fn main() -> io::Result<()> {
+    let word_list = read_wl().expect("Unable to read wordlist");
+    let count = 11;
+    let sep = "random";
+    let separators = make_separators(count, &sep.to_string());
+    let words = make_words(count, &word_list);
+    let full = make_full_phrase(&words, &separators);
+
     // TODO: Integrate with CLI library (clap?)
-    println!("{:?}", dicephrase(11, "random".to_string()));
+    println!("{:?}", full);
 
     Ok(())
 }

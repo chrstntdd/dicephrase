1fa379a581a32e42593adf1bbce5dc8324f51ed3
Christian Todd <christian.todd7@gmail.com>
2023-01-28 13:13:44 -0500

    chore: :fire: Remove astro experiment

--- apps/www2/src/features/generate/types.ts
+++
@@ -1,5 +0,0 @@
-export const ChangeCount = 0
-export const ChangeSeparator = 1
-export const Generate = 2
-
-export type Msg = typeof ChangeCount | typeof ChangeSeparator | typeof Generate
--- apps/www2/src/styles/constants.ts
+++
@@ -1,1 +0,0 @@
-export const FOCUS_RING_Z_INDEX = 42

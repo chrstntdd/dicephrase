66462482bcb5b44116c109725ce7b45d8114f975
Christian Todd <christian.todd7@gmail.com>
2023-09-24 11:45:28 -0700

    Bump gen utils deps

--- pkgs/gen-utils/vite.config.ts
+++ pkgs/gen-utils/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
 	test: {
 		coverage: {
+			provider: "v8",
 			all: true,
 			exclude: ["src/**/*.spec.ts", "src/**/*.test.ts"],
 			include: ["src"],

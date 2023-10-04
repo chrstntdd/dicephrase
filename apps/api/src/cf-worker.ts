ba97ffdc59685f7b8a1f721220d4368198c3c25d
Christian Todd <christian.todd7@gmail.com>
2023-10-03 22:42:03 -0700

    feat: :sparkles: Align on behavior to clamp entropy, use validators, update runtime deps

--- apps/api/src/cf-worker.ts
+++ apps/api/src/cf-worker.ts
@@ -1,16 +1,25 @@
 import { Hono } from "hono"
 import * as v from "@badrap/valita"
+import { valitaValidator } from "@chrstntdd/valita-validator"
+
 import {
 	combine_zip,
 	make_phrases,
 	make_separators,
-	RANDOM_SEPARATOR_OPTS,
 	VAL_RANDOM,
 } from "gen-utils"
 
 let app = new Hono()
 
-let queryParamParser = v.object({
+function clamp({ min, max }: { min: number; max: number }) {
+	return (value: number) => {
+		return Math.min(Math.max(value, min), max)
+	}
+}
+
+let clampEntropy = clamp({ min: 4, max: 20 })
+
+let schema = v.object({
 	count: v.string().chain((rawCount) => {
 		let c = +rawCount
 		if (Number.isNaN(c)) {
@@ -19,18 +28,10 @@ let queryParamParser = v.object({
 		if (!Number.isInteger(c)) {
 			return v.err(new Error("Count must be a whole number"))
 		}
-		if (c > 50) {
-			return v.err(new Error("Count is too large"))
-		}
 
-		return v.ok(c)
+		return v.ok(clampEntropy(c))
 	}),
-	sep: v.union(
-		...[
-			...RANDOM_SEPARATOR_OPTS.map((char) => v.literal(char)),
-			v.literal(VAL_RANDOM),
-		],
-	),
+	sep: v.union(v.literal(VAL_RANDOM), v.string()),
 	fmt: v
 		.union(v.literal("text"), v.literal("parts"), v.literal("verbose"))
 		.default("text"),
@@ -38,48 +39,26 @@ let queryParamParser = v.object({
 
 app.get(
 	"/gen",
-	async (ctx, next) => {
-		let rawInputQueryParams = {
-			count: ctx.req.query("count"),
-			sep: ctx.req.query("sep"),
-			fmt: ctx.req.query("fmt"),
+	// @ts-expect-error not sure about this one, the overload doesn't match?
+	valitaValidator("query", { schema }, async (r, ctx) => {
+		if (!r.success) {
+			return ctx.text("Invalid params", 400)
 		}
-		try {
-			let input = queryParamParser.parse(rawInputQueryParams)
-			ctx.req.valid(input)
-			await next()
-		} catch (error) {
-			if (error instanceof v.ValitaError && error.issues) {
-				return ctx.json(
-					{
-						// @ts-expect-error
-						// Not sure why the types are wrong, but reality is this code works.
-						issues: error.issues.map((e) => e.error.message),
-					},
-					400,
-				)
-			}
+		let { sep, fmt, count } = r.data
+		let separators = make_separators(sep, count)
+		let phrases = make_phrases(count, __WL_2016__)
 
-			return ctx.status(400)
-		}
-	},
-	async (ctx) => {
-		let input = ctx.req.valid() as v.Infer<typeof queryParamParser>
-		let separators = make_separators(input.sep, input.count)
-		let phrases = make_phrases(input.count, __WL_2016__)
-
-		if (input.fmt === "text") {
+		if (fmt === "text") {
 			return ctx.text(combine_zip(phrases, separators).join(""))
 		}
-		if (input.fmt === "parts") {
+		if (fmt === "parts") {
 			return ctx.json({
 				phrases,
 				separators,
 			})
 		}
-
-		return ctx.status(400)
-	},
+		return ctx.text("boo", 400)
+	}),
 )
 
 export default app

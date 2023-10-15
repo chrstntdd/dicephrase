1c9e423bbf482c7859acb097c74840dc51e35910
Christian Todd <christian.todd7@gmail.com>
2023-10-15 00:17:39 -0400

    OCaml perf (#54)
    * fix: :bug: Match separator string better to fix comparing "random"
    
    * Rewrite join to not depend on queue + prefer structural vs physical equality
    
    * perf: :zap: Add type annotation to clamp to avoid polymorphism
    
    * Setup vite bundling for melange
    
    * perf: :zap: Optimize JS output with melange + improve perf on native by accessing cryptokit one time per phrase generation
    
    * build: :construction_worker: Prefer docker build instead of Actions
    
    * Update dep files
    
    * Clean lockfile

--- apps/ocaml/lib/core/core.ml
+++ apps/ocaml/lib/core/core.ml
@@ -21,6 +21,7 @@ module type PlatformAdapter = sig
   module WordList : sig
     type t
 
+    val int_to_str : int -> string
     val get_word : t -> string -> string
     val load_list : unit -> t
   end
@@ -29,8 +30,9 @@ end
 type t = string list
 type separator = Char of string | Rand of string array
 
-let make_separator ~randoms str =
-  match str with "random" -> Rand randoms | str -> Char str
+let make_separator ~randoms = function
+  | str when str = "random" -> Rand randoms
+  | str -> Char str
 
 module MakeDicephrase (P : PlatformAdapter) = struct
   type wordlist = P.WordList.t
@@ -39,55 +41,49 @@ module MakeDicephrase (P : PlatformAdapter) = struct
   let init = P.Random.init
 
   let join l1 l2 =
-    let q = Queue.create () in
-    let rec combine_inner l1_rest l2_rest =
+    let rec aux acc l1_rest l2_rest =
       match (l1_rest, l2_rest) with
-      | hd1 :: tl1, hd2 :: tl2 ->
-          Queue.add hd1 q;
-          Queue.add hd2 q;
-          combine_inner tl1 tl2
-      | hd1 :: [], [] -> Queue.add hd1 q
-      | _ -> ()
+      | hd1 :: tl1, hd2 :: tl2 -> aux (hd2 :: hd1 :: acc) tl1 tl2
+      | hd1 :: _, [] -> List.rev (hd1 :: acc)
+      | _, _ -> List.rev acc
     in
-    combine_inner l1 l2;
-    List.of_seq (Queue.to_seq q)
+    aux [] l1 l2
 
   let make_wl_keys size =
     (* Generate a key by rolling 5 6-sided dice to get a key for 1 word in the wordlist *)
     let rand_key () =
       let rec aux = function
-        | acc when String.length acc == 5 -> acc
+        | acc when String.length acc = 5 -> acc
         | acc ->
-            let roll = P.Random.std_dice_roll () |> Int.to_string in
+            let roll = P.Random.std_dice_roll () |> P.WordList.int_to_str in
             aux (roll ^ acc)
       in
       aux ""
     in
-    let rec make_wl_keys_aux c acc =
-      if c == 0 then acc else make_wl_keys_aux (c - 1) (rand_key () :: acc)
+    let rec aux c acc =
+      if c = 0 then acc else aux (c - 1) (rand_key () :: acc)
     in
 
-    make_wl_keys_aux size []
+    aux size []
 
   let repeat s c =
-    let rec aux acc n = if n == 0 then acc else aux (s :: acc) (n - 1) in
+    let rec aux acc n = if n = 0 then acc else aux (s :: acc) (n - 1) in
     aux [] c
 
   let make_random_separators ~count ~separators =
+    let max = Array.length separators in
     let rec aux acc i =
-      if i == count then acc
+      if i = count then acc
       else
-        let max = Array.length separators in
         let rand_separator_idx = P.Random.get_random_int ~min:0 ~max in
         let rand_separator = Array.get separators rand_separator_idx in
         aux (rand_separator :: acc) (i + 1)
     in
     aux [] 0
 
-  let generate wl word_count sep : t =
+  let generate wl word_count sep keys : t =
     let get_from_wl = P.WordList.get_word wl in
-    let keys = make_wl_keys word_count in
-    let words = List.map get_from_wl keys in
+    let words = keys |> Array.map get_from_wl |> Array.to_list in
     let separators =
       let separator_count = word_count - 1 in
       match sep with
--- apps/ocaml/lib/native/native.mli
+++ apps/ocaml/lib/native/native.mli
@@ -1,7 +1,10 @@
 type wordlist
+type rng
 
 val init : unit -> unit
 val make_separator : randoms:string array -> string -> Core.separator
 val load_list : unit -> wordlist
-val generate : wordlist -> int -> Core.separator -> Core.t
+val generate : wordlist -> int -> Core.separator -> string array -> Core.t
 val to_string : Core.t -> string
+val make_wl_keys : rng:rng -> int -> string array
+val make_rng : unit -> rng
